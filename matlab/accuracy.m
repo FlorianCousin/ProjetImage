8a5dd8a13fb068196694460c3c4071f9e6ac84e8
Cousin Florian <florian.cousin@telecom-st-etienne.fr>
2018-01-25 16:16:44 +0100

    graph robustesse au bruit

--- matlab/accuracy.m
+++ matlab/accuracy.m
@@ -1,3 +1,9 @@
+clear;
+close all;
+clc;
 
+sigma = [0, 0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.0009, 0.001, 0.0015, 0.002, 0.0025, 0.003, 0.0035];
+precision = [40.1, 38.71, 30.65, 25.81, 24.19, 27.42, 25.81, 16.13, 19.35, 16.13, 11.29, 8.06, 8.06, 4.84, 3.23, 0];
 
-r = [
+plot(sigma, precision); title('précision en fonction de l''écart-type');
+xlabel('écart-type'); ylabel('précision');
--- matlab/papouzigrec.m
+++ matlab/papouzigrec.m
@@ -12,14 +12,16 @@ nbFiles = size(files);
 
 for i = 1:nbFiles
     I = double(imread(strcat('../comment/', files(i).name))) / 255;
-    I = imnoise(I, 'gaussian', 0, 0.0003);
+    I = imnoise(I, 'gaussian', 0, 0.003);
     neg = negatif(I);
     moy = [188.97,131.17,72.92,168.33,129.33,46.67];
     [compte, result] = ciecle(neg, I, 0.0558, moy);
-    figure; imshow(I, []); title(files(i).name);
     [m, ~] = size(result);
-    for j = 1:m
-        viscircles(result(j, 1:2), result(j, 3));
-        text(result(j, 1), result(j, 2), num2str(result(j, 4)));
+    if m > 0
+        figure; imshow(I, []); title(files(i).name);
+        for j = 1:m
+            viscircles(result(j, 1:2), result(j, 3));
+            text(result(j, 1), result(j, 2), num2str(result(j, 4)));
+        end
     end
 end
